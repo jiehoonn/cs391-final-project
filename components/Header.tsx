9425b0bbebfaa226b57f1aa2715341c17c4770b3
jocelyn mao <jocmao@bu.edu>
2025-11-21 12:01:33 -0500

    initial page layout

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center h-20">
+            <h1 className = "text-4xl font-semibold p-4">Assignment Tracker</h1>
+        </header>
+    );
+}
